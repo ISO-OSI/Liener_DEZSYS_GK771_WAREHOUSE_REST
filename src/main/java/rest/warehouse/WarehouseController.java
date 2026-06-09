3b48a9ab55208bf70c69746aebf66586e3c4175c
ISO-OSI <fliener@student.tgm.ac.at>
2026-06-09 13:44:11 +0200

    Endpunkte von /warehouse/{id}/json auf /api/warehouse, /api/warehouse.json und /api/warehouse.xml geändert; Startseite mit Links zu JSON und XML angepasst

--- src/main/java/rest/warehouse/WarehouseController.java
+++ src/main/java/rest/warehouse/WarehouseController.java
@@ -1,10 +1,9 @@
 package rest.warehouse;
 
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import rest.model.WarehouseData;
 
@@ -14,16 +13,22 @@ public class WarehouseController {
     @Autowired
     private WarehouseService service;
 	
-    @RequestMapping("/")
+    @GetMapping("/")
     public String warehouseMain() {
-    	String mainPage = "This is the warehouse application! (DEZSYS_WAREHOUSE_REST) <br/><br/>" +
-                          "<a href='http://localhost:8080/warehouse/001/json'>Link to warehouse/001/json</a><br/>";
+    	String mainPage = "This is the warehouse application! (DEZSYS_GK771_WAREHOUSE_REST) <br/><br/>" +
+                          "<a href='http://localhost:8080/api/warehouse'>JSON</a><br/>" +
+                          "<a href='http://localhost:8080/api/warehouse.xml'>XML</a><br/>";
         return mainPage;
     }
 
-    @RequestMapping(value="/warehouse/{inID}/json", produces = MediaType.APPLICATION_JSON_VALUE)
-    public WarehouseData warehouseData( @PathVariable String inID ) {
-        return service.getWarehouseData( inID );
+    @GetMapping(value = {"/api/warehouse", "/api/warehouse.json"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    public WarehouseData warehouseDataJson() {
+        return service.getWarehouseData();
+    }
+
+    @GetMapping(value = "/api/warehouse.xml", produces = MediaType.APPLICATION_XML_VALUE)
+    public WarehouseData warehouseDataXml() {
+        return service.getWarehouseData();
     }
 
 }
