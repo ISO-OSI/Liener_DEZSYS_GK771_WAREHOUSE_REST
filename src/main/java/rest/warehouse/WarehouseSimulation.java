c331cdaee847e552c6bb6605ca8ef163e9c25291
ISO-OSI <fliener@student.tgm.ac.at>
2026-06-09 13:42:05 +0200

    feste Warehouse-Daten ergänzt: Adresse, PLZ, Stadt, Land; Timestamp-Erzeugung hinzugefügt; Produktliste mit zufälligen Mengen hinzugefügt

--- src/main/java/rest/warehouse/WarehouseSimulation.java
+++ src/main/java/rest/warehouse/WarehouseSimulation.java
@@ -1,33 +1,52 @@
 package rest.warehouse;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
+import rest.model.Product;
 import rest.model.WarehouseData;
 
 public class WarehouseSimulation {
-	
-	private double getRandomDouble( int inMinimum, int inMaximum ) {
 
-		double number = ( Math.random() * ( (inMaximum-inMinimum) + 1 )) + inMinimum; 
-		double rounded = Math.round(number * 100.0) / 100.0; 
-		return rounded;
-		
+	private final Random random = new Random();
+
+	private int getRandomInt(int minimum, int maximum) {
+		return random.nextInt(maximum - minimum + 1) + minimum;
 	}
 
-	private int getRandomInt( int inMinimum, int inMaximum ) {
-
-		double number = ( Math.random() * ( (inMaximum-inMinimum) + 1 )) + inMinimum; 
-		Long rounded = Math.round(number); 
-		return rounded.intValue();
-
+	private String getCurrentTimestamp() {
+		return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new Date());
 	}
 	
-	public WarehouseData getData( String inID ) {
+	public WarehouseData getData() {
 		
 		WarehouseData data = new WarehouseData();
-		data.setWarehouseID( inID );
-		data.setWarehouseName( "Linz Bahnhof" );
+		data.setWarehouseID("001");
+		data.setWarehouseName("Linz Bahnhof");
+		data.setWarehouseAddress("Bahnhofsstrasse 27/9");
+		data.setWarehousePostalCode("4020");
+		data.setWarehouseCity("Linz");
+		data.setWarehouseCountry("Austria");
+		data.setTimestamp(getCurrentTimestamp());
+		data.setProductData(getProducts());
 
 		return data;
 		
 	}
 
+	private List<Product> getProducts() {
+		List<Product> products = new ArrayList<>();
+
+		products.add(new Product("00-443175", "Bio Orangensaft Sonne", "Getraenk", getRandomInt(1800, 3200), "Packung 1L"));
+		products.add(new Product("00-871895", "Bio Apfelsaft Gold", "Getraenk", getRandomInt(1800, 3600), "Packung 1L"));
+		products.add(new Product("01-926885", "Ariel Waschmittel Color", "Waschmittel", getRandomInt(300, 900), "Packung 3KG"));
+		products.add(new Product("00-316253", "Persil Discs Color", "Waschmittel", getRandomInt(800, 1800), "Packung 700G"));
+		products.add(new Product("02-111458", "Zewa Kuechenrolle", "Haushalt", getRandomInt(500, 1600), "Packung 4 Rollen"));
+		products.add(new Product("03-559201", "Colgate Zahnpasta Fresh", "Hygiene", getRandomInt(700, 2400), "Tube 75ML"));
+
+		return products;
+	}
 }
