8897a56d83e8d19495f842aec4902ae523a80938
ISO-OSI <fliener@student.tgm.ac.at>
2026-06-09 13:39:38 +0200

    XML-Annotationen ergänzt; neue Felder hinzugefügt: Adresse, PLZ, Stadt, Land, Produktliste; Getter/Setter für neue Felder ergänzt

--- src/main/java/rest/model/WarehouseData.java
+++ src/main/java/rest/model/WarehouseData.java
@@ -1,21 +1,27 @@
 package rest.model;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.List;
 
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+
+@JacksonXmlRootElement(localName = "warehouseData")
 public class WarehouseData {
 	
 	private String warehouseID;
 	private String warehouseName;
+	private String warehouseAddress;
+	private String warehousePostalCode;
+	private String warehouseCity;
+	private String warehouseCountry;
 	private String timestamp;
+	private List<Product> productData;
 
 	/**
 	 * Constructor
 	 */
 	public WarehouseData() {
-		
-		this.timestamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new Date());
-
 	}
 	
 	/**
@@ -37,12 +43,54 @@ public class WarehouseData {
 		this.warehouseName = warehouseName;
 	}
 
+	public String getWarehouseAddress() {
+		return warehouseAddress;
+	}
+
+	public void setWarehouseAddress(String warehouseAddress) {
+		this.warehouseAddress = warehouseAddress;
+	}
+
+	public String getWarehousePostalCode() {
+		return warehousePostalCode;
+	}
+
+	public void setWarehousePostalCode(String warehousePostalCode) {
+		this.warehousePostalCode = warehousePostalCode;
+	}
+
+	public String getWarehouseCity() {
+		return warehouseCity;
+	}
+
+	public void setWarehouseCity(String warehouseCity) {
+		this.warehouseCity = warehouseCity;
+	}
+
+	public String getWarehouseCountry() {
+		return warehouseCountry;
+	}
+
+	public void setWarehouseCountry(String warehouseCountry) {
+		this.warehouseCountry = warehouseCountry;
+	}
+
 	public String getTimestamp() {
 		return timestamp;
 	}
 
 	public void setTimestamp(String timestamp) {
 		this.timestamp = timestamp;
+	}
+
+	@JacksonXmlElementWrapper(localName = "productData")
+	@JacksonXmlProperty(localName = "product")
+	public List<Product> getProductData() {
+		return productData;
+	}
+
+	public void setProductData(List<Product> productData) {
+		this.productData = productData;
 	}
 
 	/**
